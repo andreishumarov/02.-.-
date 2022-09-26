2ca59b7c51adfbcfd56d5088f713e1bd3d2062d4
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-09-26 14:20:10 +0300

    Add files via upload

---
+++ OOP/ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            firstPerson.Name = "Vasil";
+            firstPerson.Age = 17;
+            firstPerson.IntroduceYourself();
+            firstPerson.Age = 18;
+            firstPerson.IntroduceYourself();
+            firstPerson.Name = "Marto";
+            firstPerson.Age = 19;
+            firstPerson.IntroduceYourself();
+        }
+    }
+}
