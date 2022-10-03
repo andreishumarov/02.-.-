37f41d1e819622f6f2a967c295465f382de2c397
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-10-03 14:19:04 +0300

    Add files via upload

---
+++ OOP/ex03CatTom/Cat.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex03CatTom
+{
+    class Cat
+    {
+        string name;
+        int age;
+        int length;
+        bool isMale;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+        public bool IsMale
+        {
+            get { return isMale; }
+            set { isMale = value; }
+        }
+        public Cat()
+        {
+            this.name= "Tom";
+            this.age = 2;
+            this.length = 52;
+            this.isMale = true;
+        }
+        public Cat(string name)
+        {
+            this.name = name;
+            this.age = 2;
+            this.length = 52;
+            this.isMale = true;
+        }
+        public Cat(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.length = 52;
+            this.isMale = true;
+        }
+        public Cat(string name, int age, int length)
+        {
+            this.name = name;
+            this.age = age;
+            this.length = length;
+            this.isMale = true;
+        }
+        public Cat(string name, int age, int length, bool isMale)
+        {
+            this.name = name;
+            this.age = age;
+            this.length = length;
+            this.isMale = isMale;
+        }
+        public void makeSound() => Console.WriteLine("Cat {0}, age {1} with length {2} said: Meow!", name, age, length);
+    }
+} 
---
+++ OOP/ex03CatTom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex03CatTom
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* string firstCatName = null;
+             Console.WriteLine("Write first cat name: ");
+             firstCatName = Console.ReadLine();
+             string firstCatAge = null;
+             Console.WriteLine("Write first cat age: ");
+             firstCatAge = Console.ReadLine();
+             firstCatlenght = null;
+             Console.WriteLine("Write first cat lenght: ");
+             firstCatlenght = Console.ReadLine();
+             */
+            Cat myCat01 = new Cat();
+            Cat myCat02 = new Cat("Tom");
+            Cat myCat03 = new Cat("Tom", 2);
+            Cat myCat04 = new Cat("Tom", 2, 52);
+            Cat myCat05 = new Cat("Tom", 2, 52, true);
+            myCat01.makeSound();
+            myCat02.makeSound();
+            myCat03.makeSound();
+            myCat04.makeSound();
+            myCat05.makeSound();
+        }
+    }
+}
