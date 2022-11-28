6433b89b437f59ccd9d536ee29bf3e36d53ed922
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-11-28 17:50:46 +0200

    Add files via upload

---
+++ OOP/FinalizationAndDestructorsEx4/Program.cs
@@ -0,0 +1,32 @@
+﻿class Complex
+{
+    int real, img;
+    public Complex()
+    {
+        real = 0;
+        img = 0;
+    }
+    public void SetValue()
+    {
+        real = 1;
+        img = 1;
+    }
+    public void DisplayValue()
+    {
+        Console.WriteLine("Real = " + real);
+        Console.WriteLine("Imaginary = " + img);
+    }
+    ~Complex()
+    {
+        Console.WriteLine("Destructor was called");
+    }
+    }
+class Program
+{
+    static void Main(string[]args)
+    {
+        Complex C = new Complex();
+        C.SetValue();
+        C.DisplayValue();
+    }
+}
---
+++ OOP/FinalizationAndDestructorsEx5/Program.cs
@@ -0,0 +1,51 @@
+﻿public class Student
+{
+    public string name;
+    public int  age;
+    public string  gender;
+    public string  department;
+    public Student(string name, int age, string gender, string department)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.department = department;
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+    public int getAge()
+    {
+        return this.age;
+    }
+    public string getGender()
+    {
+        return this.gender;
+    }
+    public  string getDepartment()
+    {
+        return this.department;
+    }
+    ~Student()
+    {
+        Console.WriteLine("Destructor of Student Class has been invoked");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student objStudentA = new Student("Harry", 22, "Male", "Computer Science");
+        Student objStudentB = new Student("Diana", 21, "Female", "Bisiness Studies");
+        Console.WriteLine(objStudentA.getName());
+        Console.WriteLine(objStudentA.getAge());
+        Console.WriteLine(objStudentA.getGender());
+        Console.WriteLine(objStudentA.getDepartment());
+
+        Console.WriteLine(objStudentB.getName());
+        Console.WriteLine(objStudentB.getAge());
+        Console.WriteLine(objStudentB.getGender());
+        Console.WriteLine(objStudentB.getDepartment());
+    }
+}
