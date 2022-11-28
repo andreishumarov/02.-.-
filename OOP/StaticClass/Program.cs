f6c6b39c0cbe1046fc70e1ddca6d2f33c2a2e4ee
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-11-28 17:46:10 +0200

    Add files via upload

---
+++ OOP/StaticClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+internal class Program
+{
+    private static void Main()
+    {
+        var result = Calculator.Sum(10, 25);
+        Calculator.Store(result);
+        Console.WriteLine(result);
+        var calcType = Calculator.Type;
+        Calculator.Type = "Scientific";
+    }
+}
+public static class Calculator
+{
+    private static int resultStorage = 0;
+    public static string Type = "Arithmetic";
+    public static int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    public static void Store(int result) 
+    {
+        resultStorage = result;
+    }
+}
---
+++ OOP/StaticMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static int counter = 0;
+    string name = "Demo Program";
+    static void Main(string[] args)
+    {
+        counter++;
+        Display("Hello World!");
+        //name не е зададен като стринг
+        name = "New Demo Program";
+        //Не може да я зададе като главна папка
+        SetRootFolder("D:\MyProgram");
+    }
+    static void Display(string text)
+    {
+        Console.WriteLine(text);
+    }
+    public  void SetRootFolder(string path) { }
+        }
